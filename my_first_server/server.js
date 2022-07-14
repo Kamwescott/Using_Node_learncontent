a0b79d8a394a1453375dce498c6fd4a772e01dea
Kam <Kamwescott@gmail.com>
2022-07-14 08:31:23 -0700

    second commit

--- my_first_server/server.js
+++ my_first_server/server.js
@@ -4,11 +4,19 @@ const http = require('http');
 const port = process.env.PORT||8000; //process.env = object that can read from all of the enviorment variables in our shell. 
 
 const server = http.createServer((req, res) =>{
-    const guests  = ['mary', 'Don'];
-    const guestsJSON = JSON.stringify(guests);
+   if (req.method === 'GET' && req.url === '/guests'){
+       const guests  = ['mary', 'Don'];
+       const guestsJSON = JSON.stringify(guests);
+       
+       res.setHeader('Content-Type', "application/json");
+       res.end(guestsJSON);
+   
+    } else {
+        res.statusCode = '404';
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Not found')
+   }
 
-    res.setHeader('Content-Type', "text/plain");
-    res.end(guestsJSON);
 });
 
 server.listen(port, () =>{ // telling server to listen for incoming http requests on port; 
